d6948e60270319123025be6605f868aa6b5f04a8
Parkertj298 <49212379+Parkertj298@users.noreply.github.com>
2019-11-09 18:46:32 -0500

    uploaded most recent version

--- simonClassic_Code.vhdl
+++ simonClassic_Code.vhdl
@@ -4,70 +4,84 @@ use ieee.std_logic_unsigned.all;
 use IEEE.std_logic_arith.all;
 
 ENTITY simonClassic IS
-port (clock, b0, b1, b2, b3 : IN STD_LOGIC; l0, l1,l2,l3 : OUT STD_LOGIC);
+port (clock, b0 : IN STD_LOGIC; l0, l1,l2,l3 : OUT STD_LOGIC);
 END simonClassic;
 
+
 ARCHITECTURE behavior OF simonClassic IS
-signal counter : STD_LOGIC_VECTOR(2 downto 0);
-signal randNum : STD_LOGIC_VECTOR(2 downto 0);
+signal randCount : STD_LOGIC_VECTOR(2 downto 0);
+signal halfSecCount : STD_LOGIC_VECTOR(24 downto 0);
 signal CLK_1HZ : STD_LOGIC;
-signal numOfMoves : STD_LOGIC_VECTOR(9 downto 0);
+signal randNum : STD_LOGIC_VECTOR(2 downto 0);
+signal startclock : STD_LOGIC;
+signal numOfMoves : INTEGER range 0 to 100;
 signal simonVector : STD_LOGIC_VECTOR (0 to 1);
-signal numOfInputs : STD_LOGIC;
+signal numOfInputs : INTEGER;
+
 signal userFinishedInput : STD_LOGIC;
-signal patternIncremented : STD_LOGIC;
 signal patternDisplayed : STD_LOGIC;
+
 BEGIN --behavior
-randCounter : process
+
+randCounter : process (clock)
 BEGIN -- randCounter: this will be the counter that will generate a random number
 if clock'event and clock = '1' then  -- rising clock edge
-if counter < "100" then   -- Binary value is 4
-counter <= counter + 1;
+if randCount < "100" then   -- Binary value is 4
+randCount <= randCount + 1;
 else
-counter <= (others => '0');
+randCount <= (others => '0');
 end if;
 end if;
 END process randCounter;
-patternCreator : process -- will be adding one value (two bits) to the simonVector after a signal is changed
-variable index : unsigned(10 downto 0);
+
+halfSecondClock : process
 BEGIN
-if userFinishedInput = '0' then
- randNum <= counter;
- index := STD_LOGIC_VECTOR(resize(unsigned(numOfMoves), index'length)) + unsigned(numOfMoves);
+ wait until startclock = '1';
+if clock'event and clock = '1' then  -- rising clock edge
+if halfSecCount < "1011111010111100001000000" then   -- Binary value is 4
+halfSecCount <= halfSecCount + 1;
+else
+halfSecCount <= (others => '0');
+startclock <= '0';
+end if;
+end if;
+END process halfSecondClock;
+
+createAndDisplay : process -- will be adding one value (two bits) to the simonVector after a signal is changed
+variable index : integer range 0 to 100;
+variable numOfMovesIndex : INTEGER range 0 to 100:= numOfMoves;
+SUBTYPE my_loop_range IS natural range 0 to numOfMovesIndex;
+BEGIN
+wait until userFinishedInput = '0';
+ randNum <= randCount;
+ index := numOfMoves + numOfMoves;
  simonVector(index) <= randNum(0);
  simonVector(index +1) <= randNum(1);
  numOfMoves <= numOfMoves + 1;
  userFinishedInput <= '1';
- patternIncremented <= '1';
-END if;
-END PROCESS patternCreator;
-displayPattern : process -- this will be responsible for displayign the pattern when a new element has been added
-BEGIN
-if patternIncremented = '1' then
-  for ii in 0 to numOfMoves - 1 loop
+
+  startclock <= '1';
+  for ii in my_loop_range loop
+wait until startclock = '0';
+l0 <= '0'; l1 <= '0'; l2 <= '0'; l3 <= '0';
     if simonVector((2*ii) to (2*ii) + 1) = "00" then
 l0 <= '1';
-l0 <= '0' after 1 s;
  elsif simonVector((2*ii) to (2*ii) + 1) = "01" then
      l1 <= '1';
-l1 <= '0' after 1 s;
  elsif simonVector((2*ii) to (2*ii) + 1) = "10" then
      l2 <= '1';
-l2 <= '0' after 1 s;
  elsif simonVector((2*ii) to (2*ii) + 1) = "11" then
      l3 <= '1';
-l3 <= '0' after 1 s;
  end if;
- wait for 0.5 s;
 END loop;
-patternIncremented <= '1';
 patternDisplayed <= '1';
-END if;
-END process displayPattern;
-userInput : process
+END PROCESS createAndDisplay;
+
+
+userInput : process (clock)
 BEGIN -- userInput
-if patternDiplayed = '1' then
-if b1 = '0' then
+if patternDisplayed = '1' then
+if b0 = '0' then
 userFinishedInput <= '0';
 END if;
 END if;
